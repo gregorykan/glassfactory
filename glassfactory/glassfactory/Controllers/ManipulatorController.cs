d3fde910aa1c1a9250b263f4494d885f599e38fa
gregorykan <kan.gregory@gmail.com>
2015-06-07 15:34:38 +1200

    added heaps of shit: manipulator and textEntry classes, initial seed

---
+++ glassfactory/glassfactory/Controllers/ManipulatorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using glassfactory.Models;
+
+namespace glassfactory.Controllers
+{
+    public class ManipulatorController : ApiController
+    {
+        private Manipulator Manipulator = new Manipulator();
+
+        public string GetManipulatedText(int id)
+        {
+            Manipulator.BreakIntoSentences(id);
+            return "Hello";
+        }
+    }
+}
---
+++ glassfactory/glassfactory/Models/Manipulator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace glassfactory.Models
+{
+    public class Manipulator
+    {
+        public ApplicationDbContext DbContext { get; set; }
+
+        public IEnumerable<string> BreakIntoSentences (int id)
+        {
+            TextEntry thisTextEntry = DbContext.TextEntries.FirstOrDefault(x => x.Id == id);
+            string entryBody = thisTextEntry.EntryBody;
+            return entryBody.Split('.');
+        }
+
+        public IEnumerable<IEnumerable<string>> BreakIntoWords(IEnumerable<string> sentences)
+        {
+            IEnumerable<IEnumerable<string>> sentencesInWords = new List<List<string>>();
+            foreach (string sentence in sentences)
+            {
+                
+            }
+            return sentencesInWords;
+        }
+    }
+}
